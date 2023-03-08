5c9adfc6d16c07a9e8b547c83ab6089e49f8a499
dinyoyara <dinyo.dimitrov@yara.com>
2023-03-08 12:32:25 +0200

    Set config module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -19,6 +20,9 @@ import { AuthModule } from './auth/auth.module';
             autoLoadModels: true,
             synchronize: true,
             models: [User]
+        }),
+        ConfigModule.forRoot({
+            isGlobal: true
         }),
         UsersModule,
         AuthModule
