37267def112cb80f7bee6b4f3a8e70cffffbd274
dinyoyara <dinyo.dimitrov@yara.com>
2023-03-09 16:24:23 +0200

    Implement validation in supplier create.dto.

--- src/suppliers/dto/create.dto.ts
+++ src/suppliers/dto/create.dto.ts
@@ -1,5 +1,12 @@
-export interface CreateSupplierDto {
+import { IsEmail, Length } from 'class-validator';
+
+export class CreateSupplierDto {
+    @Length(5)
     name: string;
+
+    @IsEmail()
     email: string;
+
+    @Length(8, 12)
     vat: string;
 }
