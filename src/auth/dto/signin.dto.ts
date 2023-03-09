989f3f93ff20395c0c953e99f3ac4d4f68ea42de
dinyoyara <dinyo.dimitrov@yara.com>
2023-03-09 17:06:41 +0200

    Implement validation in auth dtos.

--- src/auth/dto/signin.dto.ts
+++ src/auth/dto/signin.dto.ts
@@ -1,4 +1,9 @@
-export interface SigninDto {
+import { IsEmail, Length } from 'class-validator';
+
+export class SigninDto {
+    @IsEmail()
     email: string;
+
+    @Length(5)
     password: string;
 }
--- src/auth/dto/singup.dto.ts
+++ src/auth/dto/singup.dto.ts
@@ -1,6 +1,18 @@
-export interface SignupDto {
+import { IsEmail, IsEnum, IsOptional, Length } from 'class-validator';
+
+export class SignupDto {
+    @Length(3)
     name: string;
+
+    @IsEmail()
     email: string;
+
+    @Length(5)
     password: string;
+
+    @IsOptional()
+    @IsEnum(['admin', 'write', 'read'], {
+        message: 'role must be one of the following values: admin, read, write'
+    })
     role?: string;
 }
